e73789b3990a545bb332e0db8a5a6073f7daa4a8
Bhavani Yella <bhavaniyella@icloud.com>
2018-04-10 18:00:17 -0700

    2

--- Hello.py
+++ Hello.py
@@ -7,11 +7,16 @@
 
 
 # Dependencies
-import tweepy
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 import json
-from config import consumer_key, consumer_secret, access_token, access_token_secret
-
-
+import tweepy
+from datetime import datetime
+import time
+from config import consumer_key, consumer_secret,  access_token, access_token_secret
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
 # In[ ]:
 
 
@@ -24,11 +29,10 @@ access_token_secret = access_token_secret
 
 # In[ ]:
 
-
-# Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+api = tweepy.API(auth, wait_on_rate_limit=True)
+# Setup Tweepy API Authentication
 
 
 # In[ ]:
@@ -36,7 +40,54 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # Create a complimentary status update with a mention to another user
 #api = tweepy.API(auth)
-api.update_status("Happy Tuesday, one day closer to the weekend!!")
+target_user = "@CNN"
+def TweetOut(tweet_number):
+    counter = 1
+    sentiments = []
+    for status in tweepy.Cursor(api.search, q=target_user,tweet_mode="extended").items(100):
+        tweet = status._json
+    
+    # Run Vader Analysis on each tweet
+        compound = analyzer.polarity_scores(tweet["full_text"])["compound"]
+        pos = analyzer.polarity_scores(tweet["full_text"])["pos"]
+        neu = analyzer.polarity_scores(tweet["full_text"])["neu"]
+        neg = analyzer.polarity_scores(tweet["full_text"])["neg"]
+        tweets_ago = counter
+        
+    # Add sentiments for each tweet into an array
+        sentiments.append({"Date": tweet["created_at"], 
+                       "Compound": compound,
+                       "Positive": pos,
+                       "Negative": neu,
+                       "Neutral": neg,
+                        "Tweets Ago": tweet_number,
+                          "Tweet_text": tweet["full_text"]})
 
-#1
+    # Add to counter 
+        counter = counter + 1
+    sentiments_pd = pd.DataFrame.from_dict(sentiments)
+    plt.title("Sentiment Analysis of Tweets (%s) for %s" % (time.strftime("%x"), target_user))
+    plt.ylabel("Tweet Polarity")
+    plt.xlabel("Tweets Ago")
+    plt.plot(np.arange(len(sentiments_pd["Compound"])),
+         sentiments_pd["Compound"], marker="o", linewidth=0.5,
+         alpha=0.8)
+    ax = plt.gca().invert_xaxis()
+    plt.savefig('Sentiment Analysis.png')
+    
+repeater=0
+while(True):
+
+    # Call the TweetQuotes function and specify the tweet number
+    TweetOut(repeater)
+
+    # Once tweeted, wait 60 seconds before doing anything else
+    time.sleep(60)
+
+    # Add 1 to the counter prior to re-running the loop
+    repeater = repeater + 1
+
+    #THE END
+
+
 
